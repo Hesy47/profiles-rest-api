0e64ab680737c52d2f5b862d2c3446119f9ec222
Hesam <death19knight@gmail.com>
2024-07-11 15:35:44 +0330

    added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,9 +4,11 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register("hello-view-set", views.HelloViewSet, basename="hello-view-set")
-router.register("profile",views.UserProfileViewSet)
+router.register("profile", views.UserProfileViewSet)
+
 
 urlpatterns = [
     path("hello-api-view/", views.HelloApiView.as_view()),
+    path("login/", views.UserLoginViewSet.as_view()),
     path("", include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework import filters
 from rest_framework import status
 from profiles_api import serializers, models, permissions
 
@@ -96,3 +99,9 @@ class UserProfileViewSet(ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name", "email",)
+class UserLoginViewSet(ObtainAuthToken):
+    """Handel creating user authentication tokens"""
+    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
+    
